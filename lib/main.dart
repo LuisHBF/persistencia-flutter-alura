5ef2285fa318a8cfd981bc665531427ae60ae1f2
Luis Henrique Franco <luishbfranco@hotmail.com>
2019-12-14 20:48:40 -0300

    Ajustando o visual do Dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -7,27 +7,70 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Dashboard'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Image.asset('images/bytebank_logo.png'),
-            Container(
-              height: 100,
-              width: 100,
-              color: Colors.green,
-              child: Column(
-                children: <Widget>[
-                  Icon(Icons.people),
-                  Text('Contatos')
-                ],
+      theme: ThemeData(
+          primaryColor: Color(0xFF1B5E20),
+          accentColor: Colors.blueAccent[700],
+          buttonTheme: ButtonThemeData(
+              buttonColor: Colors.blueAccent[700],
+              textTheme: ButtonTextTheme.primary)),
+      home: Dashboard()
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/bytebank_logo.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: const BoxDecoration(
+                border: Border(
+                  top: BorderSide(width: 2.0, color: Colors.black),
+                  left: BorderSide(width: 2.0, color: Colors.black),
+                  right: BorderSide(width: 2.0, color: Colors.black),
+                  bottom: BorderSide(width: 2.0, color: Colors.black),
+                ),
+                color: Color(0xFF1B5E20)
               ),
-            )
-          ],
-        ),
+              height: 100,
+              width: 150,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(8, 15, 8, 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.people,
+                      size: 50,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      'Contatos',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
+
 }
