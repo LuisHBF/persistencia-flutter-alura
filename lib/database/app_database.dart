a0a56e4868e7524bd58e43855955f2e11cb47dc3
Luis Henrique Franco <luishbfranco@hotmail.com>
2019-12-16 14:58:23 -0300

    Utilizando async await

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -2,35 +2,30 @@ import 'package:bytebank/models/contato.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-Future<Database> criarBanco() {
-  return getDatabasesPath().then((dbPath) {
-    final String path = join(dbPath, 'bytebank.db');
-    return openDatabase(path, onCreate: (db, versao) {
-      db.execute('CREATE TABLE contatos('
-          ' id INTEGER PRIMARY KEY, '
-          ' nome TEXT,'
-          ' numero_conta INTEGER)');
-    }, version: 1);
-  });
+Future<Database> criarBanco() async {
+  return openDatabase(join(await getDatabasesPath(), 'bytebank.db'),
+      onCreate: (db, versao) {
+    db.execute('CREATE TABLE contatos('
+        ' id INTEGER PRIMARY KEY, '
+        ' nome TEXT,'
+        ' numero_conta INTEGER)');
+  }, version: 1);
 }
 
-Future<int> salvar(Contato contato) {
-  return criarBanco().then((db) {
-    Map<String, dynamic> mapContato = Map();
-    mapContato['nome'] = contato.nome;
-    mapContato['numero_conta'] = contato.numero;
-    return db.insert('contatos', mapContato);
-  });
+Future<int> salvar(Contato contato) async {
+  final Database bd = await criarBanco();
+  Map<String, dynamic> mapContato = Map();
+  mapContato['nome'] = contato.nome;
+  mapContato['numero_conta'] = contato.numero;
+  return bd.insert('contatos', mapContato);
 }
 
-Future<List<Contato>> todos() {
-  return criarBanco().then((db) {
-    return db.query('contatos').then((maps) {
-      final List<Contato> contatos = List();
-      maps.forEach((map) {
-        contatos.add(Contato(map['id'],map['nome'],map['numero_conta']));
-      });
-      return contatos;
-    });
+Future<List<Contato>> todos() async{
+  final Database bd = await criarBanco();
+  final List<Map<String,dynamic>> resultado = await bd.query('contatos');
+  final List<Contato> contatos = List();
+  resultado.forEach((map) {
+    contatos.add(Contato(map['id'], map['nome'], map['numero_conta']));
   });
+  return contatos;
 }
